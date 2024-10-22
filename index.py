b3b3f5840ef4e8f91926fed3d96905335d6b6891
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2024-10-22 17:48:10 -0500

    fix: cors

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from config import configurations  # Importa el diccionario correcto
-
+from flask_cors import CORS
 from src import init_app
 
 # Accede a la configuración de desarrollo
@@ -7,6 +7,7 @@ Configuration = configurations['development']
 
 # Inicializa la aplicación con la configuración seleccionada
 app = init_app(Configuration)
+CORS(app) 
 
 if __name__ == '__main__':
     app.run()
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from src import init_app
 from config import Settings
-
+from flask_cors import CORS
 app = init_app(Settings)
+CORS(app)
