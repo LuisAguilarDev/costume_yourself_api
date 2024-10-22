a0f7be156cbd8c54788f7bd3805a5414118b0c3b
LUIS GERARDO AGUILAR GARCIA <eagledluis.game@gmail.com>
2024-10-22 18:14:03 -0500

    Fix: all routes

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from src import init_app
 from config import Settings
-from flask_cors import CORS
+
 app = init_app(Settings)
--- src/__init__.py
+++ src/__init__.py
@@ -4,12 +4,12 @@ from flask_cors import CORS
 from src.routes import index_routes,other_routes
 
 app = Flask(__name__)
+CORS(app)
 
 
 def init_app(config):
     # Configuration
     app.config.from_object(config)
-    CORS(app, resources={r"/last_step": {"origins": "https://costume-app.vercel.app"}})
     # Blueprints
     app.register_blueprint(index_routes.main, url_prefix='/')
     app.register_blueprint(other_routes.main, url_prefix='/other')
