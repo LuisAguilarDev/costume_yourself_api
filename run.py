5717d230334ba9a05c7760652342c36cf3d92113
LUIS GERARDO AGUILAR GARCIA <100291529+LuisAguilarDev@users.noreply.github.com>
2025-01-14 14:16:54 -0500

    Feat: conda env config

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 from src import init_app
 from config import Settings
+from waitress import serve
 
 app = init_app(Settings)
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=8080)
